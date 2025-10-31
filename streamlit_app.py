e02a2f7e91055df3d6b235cd32c1fd06e9b4126c
Julio-Plan <julio.garcia@plan-international.org>
2025-10-31 00:19:08 +0000

    Update streamlit_app.py
    add import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 st.title(f":cup_with_straw: Customise your Smothie :cup_with_straw:")
 st.write(
